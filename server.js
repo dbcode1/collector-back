4a2b4cc31a08ae7784abfbdf30c84b662d12eb2b
dmb-tech <65574932+dmb-tech@users.noreply.github.com>
2022-07-27 13:12:00 -0400

    new deploy

--- server.js
+++ server.js
@@ -33,23 +33,25 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(cors()); // allows all origins
-if ((process.env.NODE_ENV = "development")) {
-  app.use(cors());
-}
+// if ((process.env.NODE_ENV = "development")) {
+//   app.use(cors());
+// }
 
 //solving cors issue
-app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "*")
-  // res.header("Access-Control-Allow-Credentials: true") 
-  res.header("Access-Control-Allow-Headers", 
-  "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Credentials: true")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   // res.header("Access-Control-Max-Age", "1000")
-  if (req.method == "OPTIONS"){
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
-      return res.status(200).json({})
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
   }
-  next()
-})
+  next();
+});
 
 // middleware
 app.use("/api", authRoutes);
