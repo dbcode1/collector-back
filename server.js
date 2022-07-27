0ac5be69bbc0b8ca662dec5e00a3e2fdf43119d8
dmb-tech <65574932+dmb-tech@users.noreply.github.com>
2022-07-27 12:52:48 -0400

    new

--- server.js
+++ server.js
@@ -34,8 +34,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(cors()); // allows all origins
 if ((process.env.NODE_ENV = "development")) {
-  app.use(cors({ origin: `http://localhost:3000` }));
+  app.use(cors());
 }
+
+//solving cors issue
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*")
+  // res.header("Access-Control-Allow-Credentials: true") 
+  res.header("Access-Control-Allow-Headers", 
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  // res.header("Access-Control-Max-Age", "1000")
+  if (req.method == "OPTIONS"){
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+      return res.status(200).json({})
+  }
+  next()
+})
+
 // middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
