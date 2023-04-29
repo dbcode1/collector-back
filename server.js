5cb1146628e9b106d39bf4ba627dca37587b534d
dmb-tech <65574932+dmb-tech@users.noreply.github.com>
2023-04-29 10:53:20 -0400

    heroku

--- server.js
+++ server.js
@@ -12,22 +12,6 @@ app.use(
   })
 );
 
-// connect to db
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials: true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  // res.header("Access-Control-Max-Age", "1000")
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
